214051be31e184f74d6171c451ae427f47a1a85e
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-03-12 22:17:53 +0300

    adding file

---
+++ scipt.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    const butt = document.getElementById("skills");
+    if (butt.style.display == "block"){
+        butt.style.display = "none";
+    }else{
+        butt.style.display = "block";
+    }
+}
